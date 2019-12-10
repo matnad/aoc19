e934d1323a4f25c78a18ed0003ef17406ac91d11
Matthias Nadler <matthias.nadler@unibas.ch>
2019-12-10 12:05:20 +0100

    day10

--- day10/day10.py
+++ day10/day10.py
@@ -5,16 +5,38 @@ from aocd.models import Puzzle
 puzzle = Puzzle(year=2019, day=10)
 in10 = puzzle.input_data.split('\n')
 
-in10 = '''#.#...#.#.
-.###....#.
-.#....#...
-##.#.#.#.#
-....#.#.#.
-.##..###.#
-..#...##..
-..##....##
-......#...
-.####.###.'''.split('\n')
+# in10 = '''#.#...#.#.
+# .###....#.
+# .#....#...
+# ##.#.#.#.#
+# ....#.#.#.
+# .##..###.#
+# ..#...##..
+# ..##....##
+# ......#...
+# .####.###.'''.split('\n')
+
+in10 = '''.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##'''.split('\n')
+
 print(in10)
 
 total = 0
@@ -24,14 +46,25 @@ for y, line in enumerate(in10):
         A[(x, y)] = char == '#'
         if char == '#':
             total += 1
-W = x
-H = y
+W = x+1
+H = y+1
 print(A)
 print(total, x, y)
 
 
 def project(a, b, d):
-    return a[0] + d * (b[0] - a[0]), a[0] + d * (b[1] - a[1])
+    if a[0] == b[0]:
+        if a[1] > b[1]:
+            d = -d
+        mod = -1 if d > 0 else 1
+        return b[0], b[1] + d + mod
+    elif a[1] == b[1]:
+        if a[0] > b[0]:
+            d = -d
+        mod = -1 if d > 0 else 1
+        return b[0] + d + mod, b[1]
+    else:
+        return a[0] + d * (b[0] - a[0]), a[1] + d * (b[1] - a[1])
 
 
 detected = defaultdict(int)
@@ -48,28 +81,41 @@ for a, isA in A.items():
         dist = 2
         while True:
             c = project(a, b, dist)
-            if c[0] > W or c[0] < 0 or c[1] > H or c[1] < 0:
+            if c[0] >= W or c[0] < 0 or c[1] >= H or c[1] < 0:
                 break
             if A[c]:
                 blocked[c] = True
+                # print(a,b,dist,c)
             dist += 1
-        dist = -2
-        while True:
-            c = project(a, b, dist)
-            if c[0] > W or c[0] < 0 or c[1] > H or c[1] < 0:
-                break
-            if A[c]:
-                blocked[c] = True
-            dist -= 1
 
     # print("blocked",len(blocked),blocked)
-    if a == (2,9):
-        print(blocked)
-    detected[a] = total - 1 - len(blocked)
+    # if a == (1,2):
+    #    print(blocked)
+    #    print(len(blocked), total - 1 - len(blocked))
 
+    detected[a] = total - 1 - len(blocked)
+    if a == (11,13):
+        print(W,H,A)
+        print(detected[a], blocked)
+        for j in range(H):
+            line = ''
+            for k in range(W):
+                if A[(k, j)]:
+                    if a == (k, j):
+                        line += 'A'
+                    elif blocked[(k, j)]:
+                        line += 'X'
+                    else:
+                        line += '#' #str(k)
+                else:
+                    line += '.'
+            print(line)
+        break
 
 srt = sorted(detected, key=detected.get, reverse=True)
+# print(detected)
 best = srt[0]
+print("total",total)
 print(best, detected[best])
 # print(project((0, 0), (3, 1), -2))
-# puzzle.answer_a =
+# puzzle.answer_a = detected[best]
